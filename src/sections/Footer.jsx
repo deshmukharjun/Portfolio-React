904f2ebdfda7e3fd8be77168d598e5df07649227
Arjun <arjun_deshmukh_aiml@moderncoe.edu.in>
2025-02-05 00:32:08 +0530

    Updated the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,12 +6,17 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>  
+        <a href="https://github.com/deshmukharjun" target="_blank" rel="noopener noreferrer">
+          <div className="social-icon">
+            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/arjun-deshmukh-629841227/" target="_blank" rel="noopener noreferrer">
+          <div className="social-icon">
+            <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2" />
+          </div>
+        </a>
+ 
       </div>
 
       <p className="text-white-500">All rights reserved.</p>
